363856dcf4ae096c9fad5c31f6a1fcdbad5a6993
Harsh <harshrathore3101@gmail.com>
2023-01-05 18:21:29 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+import VideoDetail from "./Components/VideoDetail";
+import ChannelDetail from "./Components/ChannelDetail";
+import SearchFeed from "./Components/SearchFeed";
+import Feed from "./Components/Feed";
 
 const App = () => {
   return (
-    <div>Start app</div>
-  )
-}
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:search" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  );
+};
 
-export default App
+export default App;
